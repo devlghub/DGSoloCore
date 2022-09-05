f1e62683c10504793f9b940d2a2e57f8e8317334
Liam Gomez <liam.gomez@daznbet.com>
2022-09-05 12:29:47 +0200

    Added UI. Changed movement of hud on css.

---
+++ [SoloRP]/[Menus]/f1_menu_system/fxmanifest.lua
@@ -0,0 +1,14 @@
+-- Resource Metadata
+fx_version 'cerulean'
+game 'gta5'
+
+author 'DarthGaijin'
+description 'Start to getting user to writing some script for GTA FiveM'
+version '1.0.0'
+
+-- What to run
+-- files {
+-- }
+
+server_script "source/server.lua"
+client_script "source/client.lua"
